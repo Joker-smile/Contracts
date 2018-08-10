bb47d01f13800fd1775dcfa435f40d2af42bc969
joker <zai1020733278@gmail.com>
2018-08-10 14:55:30 +0800

    fix

---
+++ Repositories/Criterias/WhereCriteria.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Contracts\Repositories\Criterias;
+
+use App\Contracts\Repositories\CriteriaInterface;
+use App\Contracts\Repositories\RepositoryInterface;
+
+class WhereCriteria implements CriteriaInterface
+{
+    protected $column;
+
+    protected $operator;
+
+    protected $value;
+
+    protected $boolean;
+
+    public function __construct($column, $operator = null, $value = null, $boolean = 'and')
+    {
+        $this->column = $column;
+
+        $this->operator = $operator;
+
+        $this->value = $value;
+
+        $this->boolean = $boolean;
+    }
+
+    public function apply($model, RepositoryInterface $repository)
+    {
+        if ($this->operator == 'in') {
+            return $model->whereIn($this->column, $this->value, $this->boolean);
+        }
+
+        if ($this->operator == 'between') {
+            return $model->whereBetween($this->column, $this->value, $this->boolean);
+        }
+
+        if ($this->operator == 'has') {
+            return $model->whereHas($this->column, $this->value);
+        }
+
+        if ($this->operator == 'date') {
+            return $model->whereDate($this->column, $this->value);
+        }
+
+        return $model->where($this->column, $this->operator, $this->value, $this->boolean);
+    }
+}
